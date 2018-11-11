36589e46c86671a5617cde93211db470e9303553
Fred Campos <fcf.camposx@gmail.com>
2018-11-11 14:55:33 -0400

    removed api keys

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,14 +2,6 @@ import firebase from 'firebase/app';
 import firestore from 'firebase/firestore/';
 // Initialize Firebase
 
-var config = {
-  apiKey: 'AIzaSyBNSP_dBVJOzhK7oUB_sNLqwE_mkJ-TNY4',
-  authDomain: 'smoothie-maker-5a340.firebaseapp.com',
-  databaseURL: 'https://smoothie-maker-5a340.firebaseio.com',
-  projectId: 'smoothie-maker-5a340',
-  storageBucket: 'smoothie-maker-5a340.appspot.com',
-  messagingSenderId: '143167629269'
-};
 const firebaseApp = firebase.initializeApp(config);
 firebaseApp.firestore().settings({ timestampsInSnapshots: true });
 // export firestore database
