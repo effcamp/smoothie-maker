3305af0e77b2e150ba235a8edaae204796437d63
Fred Campos <fcf.camposx@gmail.com>
2018-11-11 15:09:21 -0400

    Fixed env variables.

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,7 +1,15 @@
 import firebase from 'firebase/app';
 import firestore from 'firebase/firestore/';
+import * as config from './dev.env.js';
 // Initialize Firebase
-
+var config = {
+  apiKey: config.apiKey || process.env.API_KEY,
+  authDomain: config.authDomain || process.env.AUTH_DOMAIN,
+  databaseURL: config.databaseURL || process.env.DATABASE_URL,
+  projectId: config.projectId || process.env.PROJECT_ID,
+  storageBucket: config.storageBucket || process.env.STORAGE_BUCKET,
+  messagingSenderId: config.storageBucket || process.env.MESSAGING_SENDER_ID
+};
 const firebaseApp = firebase.initializeApp(config);
 firebaseApp.firestore().settings({ timestampsInSnapshots: true });
 // export firestore database
