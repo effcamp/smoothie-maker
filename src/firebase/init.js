82e43fbb7dfa8358441b65e174bae426c524372c
Fred Campos <fcf.camposx@gmail.com>
2018-11-11 15:12:14 -0400

    Fixed typo

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 import firestore from 'firebase/firestore/';
 import * as config from './dev.env.js';
 // Initialize Firebase
-var config = {
+const keys = {
   apiKey: config.apiKey || process.env.API_KEY,
   authDomain: config.authDomain || process.env.AUTH_DOMAIN,
   databaseURL: config.databaseURL || process.env.DATABASE_URL,
@@ -10,7 +10,7 @@ var config = {
   storageBucket: config.storageBucket || process.env.STORAGE_BUCKET,
   messagingSenderId: config.storageBucket || process.env.MESSAGING_SENDER_ID
 };
-const firebaseApp = firebase.initializeApp(config);
+const firebaseApp = firebase.initializeApp(keys);
 firebaseApp.firestore().settings({ timestampsInSnapshots: true });
 // export firestore database
 
